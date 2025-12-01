40fbea9237add34746cd126947fe526a16d3cdd4
nwejin <147528674+nwejin@users.noreply.github.com>
2025-12-01 23:25:19 +0900

    use dynamic import for phaser to avoid SSR error

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,25 @@
 'use client';
 
 import { useEffect, useRef } from 'react';
-import * as Phaser from 'phaser';
-import Config from '../games/Config';
 
 export default function Home() {
   const gameContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (gameContainerRef.current) {
-      new Phaser.Game({
-        ...Config,
-        parent: gameContainerRef.current,
-      });
+      initializeGame();
     }
   }, []);
+
+  async function initializeGame() {
+    const Phaser = await import('phaser');
+    const Config = (await import('../games/Config')).default;
+
+    new Phaser.Game({
+      ...Config,
+      parent: gameContainerRef.current,
+    });
+  }
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
