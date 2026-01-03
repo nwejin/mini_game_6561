25564cd338522cde7b6c5255b9bfddf63ea23de8
nwejin <147528674+nwejin@users.noreply.github.com>
2026-01-03 18:34:08 +0900

    update: routing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/game/6561',
+  assetPrefix: '/game/6561',
 };
 
 export default nextConfig;
