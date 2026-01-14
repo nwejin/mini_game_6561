4ab72510f5007bf961e430f9db37be511976dbdd
nwejin <147528674+nwejin@users.noreply.github.com>
2026-01-14 18:01:37 +0900

    update: new game

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,12 @@ export default function Home() {
       <Link href="/6561">
         <button className="bg-blue-500 px-10 py-3 rounded-xl text-white text-xl font-bold">GAME START - 6561</button>
       </Link>
+
+      <Link href="/enhance">
+        <button className="bg-green-500 px-10 py-3 rounded-xl text-white text-xl font-bold">
+          GAME START - 강화x강화
+        </button>
+      </Link>
     </div>
   );
 }
